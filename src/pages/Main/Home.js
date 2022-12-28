79508b00a69f5b0647e7ec9cf05d9ad51953f8e3
ShahariarRahman <shahariarrahman98@gmail.com>
2022-12-28 07:21:59 +0600

    fetch product by redux thunk

--- src/pages/Main/Home.js
+++ src/pages/Main/Home.js
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import ProductCard from "../../components/ProductCard";
+import fetchProductData from "../../redux/thunk/products/fetchProducts";
 
 const Home = () => {
-  const [products, setProducts] = useState([]);
+  const products = useSelector(state => state.product.products);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    fetch("http://localhost:5000/product")
-      .then((res) => res.json())
-      .then((data) => setProducts(data));
-  }, []);
+    dispatch(fetchProductData());
+  }, [dispatch]);
 
   const activeClass = "text-white  bg-indigo-500 border-white";
 
@@ -29,7 +30,7 @@ const Home = () => {
       </div>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-14'>
         {products.map((product) => (
-          <ProductCard key={product.model} product={product} />
+          <ProductCard key={product._id} product={product} />
         ))}
       </div>
     </div>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,11 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
 import rootReducer from "./reducers/rootReducer";
+import thunk from "redux-thunk";
 
-const store = createStore(rootReducer, composeWithDevTools());
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk)),
+);
 
 export default store;
