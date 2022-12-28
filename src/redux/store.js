0995363a5c17a7e5897b23bf558cecb6d168b467
ShahariarRahman <shahariarrahman98@gmail.com>
2022-12-28 09:29:49 +0600

    sort cart by custom middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,11 @@ import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
 import rootReducer from "./reducers/rootReducer";
 import thunk from "redux-thunk";
+import cartCounter from "./middleware/cartCounter";
 
 const store = createStore(
     rootReducer,
-    composeWithDevTools(applyMiddleware(thunk)),
+    composeWithDevTools(applyMiddleware(thunk, cartCounter)),
 );
 
 export default store;
